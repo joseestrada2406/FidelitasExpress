969c943a1cf5178357a24bf5d7db0d92da7564e3
Jose Andres Estrada <pepe100824@gmail.com>
2022-07-12 00:17:14 -0600

    Creacion de subprogramas e implementacion de listas

--- avance2.py
+++ avance2.py
@@ -1,79 +1,117 @@
-#Inicio proyecto Fidelitas Express
-#Constantes
-
 COSTO_INICIAL = 1000
 PESO_MAXIMO = 45
 MODULO_EXPRESS = 200
 MODULO_BAJO_COSTO = 100
 MODULO_INTERNACIONAL = 300
 REDUCCION_PESO = 1
+identificador_paquete = 0
+listaPedidos = []
 
-#Bienvenida
-print("Bienvenido a envíos Fidélitas express")
-print("Somos una empresa de hacer envios donde usted los necesite \n")
+def flujoPrincipalProyecto():
+    SALIDA = 0
+    seleccionModulo = 0
+    bienvenida()
+    while SALIDA == 0:
+        menuModulos()
+        seleccionModulo = int(input("Seleccione el módulo al que desea acceder: "))
+        if seleccionModulo == 1:
+            moduloEnvio()
+        SALIDA +=1
+    
 
-#Datos de entrada
-
-#Modalidades de envio
-print("Se cuenta con 3 modalidades de envío:")
-print("Express")
-print("Bajo costo")
-print("Internacional\n")
-
-#Imprimir costos de los envios
-print("Cada paquete tiene un costo inicial de ₡1000 por el primer Kg")
-print("Costo por Kilogramo adicional según la modalidad seleccionada: \n")
-
-print("1. Express: ₡200 por kilogramo")
-print("2. Bajo costo: ₡100 por kilogramo")
-print("3. Internacional: ₡300 por kilogramo\n")
-
-#variable para ejecutar el while
-salida = 0
-paquetes = 0
-
-while salida < 2:
-    if salida <= 1:
-#Selección de modalidad y peso de primer paquete
-             paquetes = paquetes + 1
-             print("Paquete #", paquetes)
-             modalidad_seleccionada = int(input("\nSeleccione el número de la modalidad de envío: ")) 
-
-    if modalidad_seleccionada <= 3:
+def bienvenida():
+    #Bienvenida
+    print("Bienvenido a envíos Fidélitas express")
+    print("Somos una empresa de hacer envios donde usted los necesite \n")
+    
+def menuModulos():
+    print("Contamos con las siguientes opciones para usted: \n")
+    print("1. Módulo de Envío")
+    print("2. Módulo de Facturación")
+    print("3. Módulo de Informes\n")
+    
+def moduloEnvio():
+    SALIDA = 0
+    print("Ingrese sus datos para continuar: ")
+    informacionUsuario = solicitarInformacionParaEnvio()
+    imprimirGeneralidadesEnvio()
+    while SALIDA < 2:
+        menuModalidadesEnvio()
+        global identificador_paquete
+        identificador_paquete +=1
+        print("Número de pedido: ", identificador_paquete)
+        modalidad_seleccionada = int(input("\nSeleccione el número de la modalidad de envío: ")) 
+        if modalidad_seleccionada <= 3:
                 peso_paquete = float(input("Ingrese el Peso(kg) del paquete: "))
+                if peso_paquete <= float(PESO_MAXIMO):
+                    if modalidad_seleccionada == 1:
+                        print("\nSe aplicará el cargo de ₡200 por Kg")
+                        costo_por_kilogramo = COSTO_INICIAL + (MODULO_EXPRESS * (peso_paquete - REDUCCION_PESO))
+                        print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
+                        informacionUsuario.insert(0, identificador_paquete)
+                        informacionUsuario.append("Express")
+                        informacionUsuario.append(costo_por_kilogramo)
+                        listaPedidos.append(informacionUsuario)
+                        print(listaPedidos)
+                        print("\nSu envio fue registrado")
+                        print("Desea ingresar otro pedido?")
+                        SALIDA = int(input("1 = Si / 2 = No \n"))
+                        
 
-#Calculo del precio a pagar
-    if peso_paquete <= float(PESO_MAXIMO):
-            salida == 2 
-            if modalidad_seleccionada == 1:
+                    elif modalidad_seleccionada == 2:
+                        print("\nSe aplicará el cargo de ₡100 por Kg")
+                        costo_por_kilogramo = COSTO_INICIAL + (MODULO_BAJO_COSTO * (peso_paquete - REDUCCION_PESO))
+                        print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
+                        informacionUsuario.insert(0, identificador_paquete)
+                        informacionUsuario.append("Bajo costo")
+                        informacionUsuario.append(costo_por_kilogramo)
+                        listaPedidos.append(informacionUsuario)
+                        print(listaPedidos)
+                        print("\nSu envio fue registrado")
+                        print("Desea ingresar otro pedido?")
+                        SALIDA = int(input("1 = Si / 2 = No \n"))
 
-                print("\nSe aplicará el cargo de ₡200 por Kg")
-                costo_por_kilogramo = COSTO_INICIAL + (MODULO_EXPRESS * (peso_paquete - REDUCCION_PESO))
-                print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
-                print("\nSu envio fue registrado")
-                print("Desea ingresar otro pedido?")
-                salida = int(input("1 = Si / 2 = No \n"))
-                
+                    elif modalidad_seleccionada == 3:
+                        print("\nSe aplicará el cargo de ₡300 por Kg")
+                        costo_por_kilogramo = COSTO_INICIAL + (MODULO_INTERNACIONAL * (peso_paquete - REDUCCION_PESO))
+                        print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
+                        informacionUsuario.insert(0, identificador_paquete)
+                        informacionUsuario.append("Internacional")
+                        informacionUsuario.append(costo_por_kilogramo)
+                        listaPedidos.append(informacionUsuario)
+                        print(listaPedidos)
+                        print("\nSu envio fue registrado")
+                        print("Desea ingresar otro pedido?")
+                        SALIDA = int(input("1 = Si / 2 = No \n")) 
+                else:            
+                    print("El peso máximo permitido son 45 kg")
+        else:
+            print('Opción ingresada no es válida\n')
+    else:
+            print("Muchas gracias por realizar el envio con nosotros")
 
-            elif modalidad_seleccionada == 2:
-                 print("\nSe aplicará el cargo de ₡100 por Kg")
-                 costo_por_kilogramo = COSTO_INICIAL + (MODULO_BAJO_COSTO * (peso_paquete - REDUCCION_PESO))
-                 print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
-                 print("\nSu envio fue registrado")
-                 print("Desea ingresar otro pedido?")
-                 salida = int(input("1 = Si / 2 = No \n"))
+#Función para guardar los datos ingresados por el usuario
+def solicitarInformacionParaEnvio():
+    nombre = input("Nombre completo: ")
+    identificación = int(input("Identificación: "))
+    empresa = input("Empresa donde labora: ")
+    informacionPersona = [nombre,identificación,empresa]
+    return informacionPersona
 
-            elif modalidad_seleccionada == 3:
-                print("\nSe aplicará el cargo de ₡300 por Kg")
-                costo_por_kilogramo = COSTO_INICIAL + (MODULO_INTERNACIONAL * (peso_paquete - REDUCCION_PESO))
-                print("El costo por el envío del paquete es de:", "₡", costo_por_kilogramo)
-                print("\nSu envio fue registrado")
-                print("Desea ingresar otro pedido?")
-                salida = int(input("1 = Si / 2 = No \n")) 
-    else:            
-        print("El peso máximo permitido son 45 kg")
 
-else:
-         print("Muchas gracias por realizar el envio con nosotros")
- 
-#Fin
+def imprimirGeneralidadesEnvio():
+    print("\nSe cuenta con 3 modalidades de envío:")
+    print("Express")
+    print("Bajo costo")
+    print("Internacional\n")
+    #Imprimir costos de los envios
+    print("Cada paquete tiene un costo inicial de ₡1000 por el primer Kg.")
+    print("Costo por Kilogramo adicional según la modalidad seleccionada: \n")
+    
+    
+def menuModalidadesEnvio():
+    print("1. Express: ₡200 por kilogramo")
+    print("2. Bajo costo: ₡100 por kilogramo")
+    print("3. Internacional: ₡300 por kilogramo\n")
+    
+flujoPrincipalProyecto()
--- file1.py
+++ file1.py
@@ -1,3 +1,25 @@
-print("Hello Erick")
-print ("Hello Juan")
-print()
+listaPedidos = []
+
+def agregarUsuarios(item):
+    listaPedidos.append(item)
+    
+
+def solicitarInformacionParaEnvio():
+    nombre = input("Nombre completo: ")
+    identificación = int(input("Identificación: "))
+    empresa = input("Empresa donde labora: ")
+    informacionPersona = [nombre,identificación,empresa]
+    print(informacionPersona)
+    return informacionPersona
+
+
+def pruebaLista():
+    prueba = solicitarInformacionParaEnvio()
+    #print(prueba)
+    print(listaPedidos)
+    valor = input("Ingrese el valor: ")
+    prueba.insert(0, valor)
+    agregarUsuarios(prueba)
+    print(prueba)
+    print(listaPedidos)
+pruebaLista()
